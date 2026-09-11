6fc7bea4bfbf443f94553ea4446115d772286fd3
Doulglasz <gustavomayerdasilva434@gmail.com>
2026-09-11 10:37:11 -0300

    Adicionado mais alguns basicos

--- Basico/script.js
+++ Basico/script.js
@@ -1,3 +1,46 @@
-var cidade = "São Paulo";
-var cidade = "Rio de Janeiro";
-console.log(cidade)
+// exercicio 1
+    var cidade = "São Paulo";
+    var cidade = "Rio de Janeiro";
+    console.log(cidade);
+
+// exercicio 2
+    let pais = "Brasil";
+    //let pais = "Paris";
+
+// exercicio 3
+    const PI = 3.14;
+    //PI = 4;
+
+// exercicio 4
+    const animais = ["cachorro", "gato"];
+    animais.push("porco");
+    console.log(animais);
+
+// exercicio 5
+    let idade = 20;
+    let carteira = false;
+
+    if(idade >= 18 && carteira){
+        console.log("Pode dirigir");
+    }else{
+        console.log("Não pode dirigir");
+    }
+
+// exercicio 6
+    let feriado = false;
+    let fimDeSemana = true;
+
+    if(feriado || fimDeSemana){
+        console.log("Dia de descanso");
+    }else{
+        console.log("Dia de trabalho");
+    }
+
+// exercicio 7
+    let chuva = false;
+
+    if(!chuva){
+        console.log("Podemos ir para a praia");
+    }else{
+        console.log("Ficaremos em casa");
+    }
